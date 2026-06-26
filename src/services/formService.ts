ae09f11a121e8b4f7737959ea072a1691e747510
shrutii-29 <shrutishreya156@gmail.com>
2026-06-27 02:41:16 +0530

    Added form service

---
+++ src/services/formService.ts
@@ -0,0 +1,11 @@
+export const createForm = async () => {
+  console.log("Create form");
+};
+
+export const getForms = async () => {
+  console.log("Get forms");
+};
+
+export const deleteForm = async (id: string) => {
+  console.log("Delete form:", id);
+};
